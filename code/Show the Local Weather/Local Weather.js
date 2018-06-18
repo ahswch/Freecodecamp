d8b481169aec92dea5a5c73ebafcc5f1a94370f8
ahswch <qq5372@live.com>
2018-06-18 23:04:38 +0800

    项目Show the Local Weather基本功能完成
    界面待优化

--- code/Show the Local Weather/Local Weather.js
+++ code/Show the Local Weather/Local Weather.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
-    $(".bb").click(function(){
-      alert("未显示天气是因为所用天气api不支持跨域，需安装跨域插件才能显示天气，安装地址：https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=zh-CN")
-    })
-    var lat;
-    var lon;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
           $(".l").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
           lat = position.coords.latitude;
           lon = position.coords.longitude;
-          $.getJSON("http://v.juhe.cn/weather/geo?lon=117.82271199999998&lat=32.891617499999995&format=&dtype=json&key=bf4796cf557b98cd1faa0115aa836025",function(json){
-            $(".d").html(json.result.today[ "date_y"]);
+      });
+      }
+      $(".bb").click(function(){
+        $.ajax({
+          url: "https://v.juhe.cn/weather/geo?lon="+lon+"&lat=" +lat+ "&format=2&key=bf4796cf557b98cd1faa0115aa836025",
+          type: "get",
+          dataType: "jsonp",
+          success: function(json){
+            var future = json.result.future;
+            $(".d").html(json.result.today.date_y+" "+json.result.today.week);
             $(".c").html(json.result.today.city);
             $(".de").html(json.result.today.temperature+' '+json.result.today.weather+' '+json.result.today.wind);
-            var a ='"'+Object.getOwnPropertyNames(json.result.future)[0]+'"';
-          });
-         
+            for(var i =1;i<4;i++){
+            var day = "#day" + i;
+            $(day).html("<p>天气:" + future[i].weather + "</p><p>气温:" + future[i].temperature + "</p><p>" + future[i].date + "</p><p>" + future[i].week + "</p>");
+            }
+          },
+          error: function(){
+            alert("error 天气数据通过经纬度定位获得，请确保您已允许此网页使用定位权限");
+          }
+          
         });
-        
-      }
-
+    });
 });
